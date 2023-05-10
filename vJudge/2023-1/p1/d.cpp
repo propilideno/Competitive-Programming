1eb42345bb19ec65921d71ac4b9ea73a615bb54c
Lucas <propilideno@gmail.com>
2023-05-10 15:09:27 -0300

    Update

--- vJudge/2023-1/p1/d.cpp
+++ vJudge/2023-1/p1/d.cpp
@@ -1,21 +1,4 @@
-#include <array>
 #include <bits/stdc++.h>
-
-#define DEBUG false
-#define OJ_DEBUG
-
-#define $(x)                                                                   \
-  {                                                                            \
-    if (DEBUG) {                                                               \
-      cout << __LINE__ << ": ";                                                \
-      {x} cout << endl;                                                        \
-    }                                                                          \
-  }
-#define _(x)                                                                   \
-  { cout << #x << " = " << x << " "; }
-
-const double E = 1e-8;
-const double PI = acos(-1);
 
 using namespace std;
 
@@ -23,16 +6,30 @@ int main() {
   ios::sync_with_stdio(false);
   int n;
   cin >> n;
-  int j = n;
-  vector<int> k;
-  int temp;
-  while (n--) {
-    cin >> temp;
-    k.push_back(temp);
+  vector<int> k(n);
+  for (int i = 0; i < n; i++) {
+    cin >> k[i];
   }
-  sort(k.begin(), k.end());
-  int p = k[0];
-  int q = k[n - 1];
-  for (int i = 0; i < n / 2; p = k[i]) {
+  sort(k.begin(), k.end()); // Ordena o array
+
+  auto left = k.begin();           // Começa no começo
+  auto middle = k.begin() + n / 2; // Iterador pro meio do array
+
+  int numberOf_Clusters = 0;
+  int numberOf_Alones = 0;
+  if (n % 2) {
+    numberOf_Alones++; // O do meio fica sozinho
+    middle++;          // O meio passa a ser o próximo
   }
+  for (auto right = middle; right <= k.end(); right++) {
+    if (2 * (*left) <= *right) { // Pelo menos o dobro.
+                                 // Quer dizer que o dobro vale.
+      numberOf_Clusters++;
+      left++;
+    } else {
+      numberOf_Alones++;
+    }
+  }
+
+  cout << numberOf_Clusters + numberOf_Alones << endl;
 }
--- vJudge/2023-1/p1/e.cpp
+++ vJudge/2023-1/p1/e.cpp
@@ -2,11 +2,6 @@
 
 using namespace std;
 
-// # Método Recursivo
-// # Constrói uma SegTree 'T' a partir de um array 'A'
-// # O primeiro elemento de 'A' está em 'start'
-// # O último elemenot de 'A' está em 'end'
-// # O nó atual da árvore sendo analisado é 'node'
 void buildRec(int *T, int *A, int node, int start, int end) {
   if (start == end) {
     // # Folha representa um elemento único
@@ -21,24 +16,11 @@ void buildRec(int *T, int *A, int node, int start, int end) {
     T[node] = T[2 * node] + T[2 * node + 1]; // # <======= IMPORTANTE
   }
 }
-// # Interface para a construção da árvore
-// # 'T' é o vetor que armazenará a árvore
-// # 'A' é o vetor com os dados originais
-// # 'n' é o número de elemento em 'A'. 'T' deve ter tamanho '2n'
-// # Complexidade de tempo: O(n)
-// # Complexidade de espaço: O(n)
 void build(int *T, int *A, int n) {
   buildRec(T, A, 1, 0,
            n - 1); // # A raíz tem nó 1 e representa o segmento A[0, ..., n-1]
 }
 
-// # Função recursiva pela busca da soma de A[l, ..., r]
-// # 'T' é a SegTree
-// # 'node' é o nó atual da SegTree sendo inspecionado
-// # 'start' é o início do intervalo representado em 'node'
-// # 'end' é o fim do intervalo representado em 'node'
-// # 'l' é o limite inferior da busca
-// # 'r' é o limite superior da busca
 int queryRec(int *T, int node, int start, int end, int l, int r) {
   if (r < start or end < l) {
     // # [start, end] está fora de [l, r] -- não há interseção
@@ -54,23 +36,10 @@ int queryRec(int *T, int node, int start, int end, int l, int r) {
   int p2 = queryRec(T, 2 * node + 1, mid + 1, end, l, r);
   return (p1 + p2); // # <======= IMPORTANTE
 }
-// # Interface para a busca: Soma dos elementos A[l, ..., r]
-// # 'T' é a SegTree
-// # 'n' é o número de elementos no vetor 'A'
-// # 'l' limite inferior da busca
-// # 'r' limite superior da busca
 int query(int *T, int n, int l, int r) { // #Soma(A[l, ..., r])
   return queryRec(T, 1, 0, n - 1, l, r);
 }
 
-// # Função Recursiva para atualizar A[idx], fazendo A[idx] += val
-// # 'T' é a SegTree
-// # 'A' é o vetor original com os elementos
-// # 'node' é o nó atual da SegTree sendo inspecionado
-// # 'start' é o início do intervalo representado por 'node'
-// # 'end' é o fim do intervalo representado por 'node'
-// # 'idx' é o índice do elemento a ser atualizado
-// # 'val' é o valor a ser somado a A[idx]
 void updateRec(int *T, int *A, int node, int start, int end, int idx, int val) {
   if (start == end) {
     // # Nó folha, atualiza A e T
@@ -89,12 +58,6 @@ void updateRec(int *T, int *A, int node, int start, int end, int idx, int val) {
     T[node] = T[2 * node] + T[2 * node + 1]; // # <======= IMPORTANTE
   }
 }
-// # Interface para atualizar A[idx], fazendo A[idx] += val
-// # 'T' é a SegTree
-// # 'A' é o vetor original com os elementos
-// # 'n' é o tamanho de 'A'
-// # 'idx' é o índice do elemento a ser atualizado
-// # 'val' é o valor a ser somado a A[idx]
 void update(int *T, int *A, int n, int idx, int val) { // # A[idx] = A[idx] +
                                                        // val
   updateRec(T, A, 1, 0, n - 1, idx, val);
